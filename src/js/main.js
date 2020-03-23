1b8265c1cba3ac0f26a7f7520617e6107434e30b
Savelic4h <gsavelich@mail.ru>
2020-03-23 09:43:15 +0700

    fixed slider

--- src/js/main.js
+++ src/js/main.js
@@ -5,15 +5,32 @@ $(function () {
   $('.form-conditions').on('click', function () {
     $(this).toggleClass('active')
   });
-  $('.ourWorks__examples-slider').slick({
+  $('.ourWorks__examples-slider-left').slick({
     arrows: false,
-    asNavFor: '.ourWorks__examples-slider-preview'
+    asNavFor: '.ourWorks__examples-slider-preview-left',
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    fade: true,
   });
-  $('.ourWorks__examples-slider-preview').slick({
+  $('.ourWorks__examples-slider-preview-left').slick({
     arrows: false,
     slidesToShow: 3,
-    slidesToScroll: 3,
-    asNavFor: '.ourWorks__examples-slider',
+    slidesToScroll: 1,
+    asNavFor: '.ourWorks__examples-slider-left',
+    focusOnSelect: true
+  });
+  $('.ourWorks__examples-slider-right').slick({
+    arrows: false,
+    asNavFor: '.ourWorks__examples-slider-preview-right',
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    fade: true,
+  });
+  $('.ourWorks__examples-slider-preview-right').slick({
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.ourWorks__examples-slider-right',
     focusOnSelect: true
   });
 });
